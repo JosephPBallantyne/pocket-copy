dc4905c55eebcab4c069ce3081600adba8b119f4
josephpballantyne@gmail.com <josephpballantyne@gmail.com>
2024-10-15 22:00:53 +0100

    Initial Commit

---
+++ PocketCopy/PocketCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCopyApp.swift
+//  PocketCopy
+//
+//  Created by Joe Ballantyne on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PocketCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PocketCopyTests/PocketCopyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCopyTests.swift
+//  PocketCopyTests
+//
+//  Created by Joe Ballantyne on 15/10/2024.
+//
+
+import Testing
+@testable import PocketCopy
+
+struct PocketCopyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
